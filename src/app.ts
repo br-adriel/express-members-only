faf410cb1971b014c4d533355a84d534cebc77da
Adriel <adriel.fsantos@outlook.com>
2023-02-27 21:48:54 -0300

    Configuração inicial

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import express from 'express';
+import helmet from 'helmet';
+import logger from 'morgan';
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+
+/* Definindo template engine */
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+/* Middlewares */
+app.use(logger('dev'));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        imgSrc: [`'self'`, `data:`],
+        scriptSrc: [`'self'`, `'unsafe-eval'`, `'unsafe-inline'`],
+      },
+    },
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('../public'));
+app.use(compression());
+
+/* Iniciando o servidor */
+app.listen(PORT, () => {
+  console.log(`SERVER RUNNING => http://localhost:${PORT}`);
+});
