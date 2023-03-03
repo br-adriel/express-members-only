962d2668306fceb555ac06fcfc90728987be2b37
Adriel <adriel.fsantos@outlook.com>
2023-03-03 18:43:36 -0300

    Implementa atualização de usuário

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import User from '../models/User';
 
 /** Exibe perfil do usuário */
@@ -26,19 +27,77 @@ export const get_user_profile = async (
 };
 
 /** Exibe formulário de edição do usuário */
-export const get_user_profile_edit = async (
+export const get_user_profile_edit = (
   req: Request<{ username: string }>,
   res: Response,
   next: NextFunction
 ) => {
-  return res.send(`EDIT USER "${req.params.username}" PROFILE`);
+  if (req.params.username !== req.user!.username) {
+    return res.render('errorPage', {
+      error: {
+        status: 403,
+        message: 'Você não tem permissões para editar esse usuário',
+      },
+    });
+  }
+  return res.render('users/editProfile', {
+    formData: {
+      firstName: req.user!.firstName,
+      lastName: req.user!.lastName,
+    },
+  });
 };
 
 /** Salva as modificações feitas no perfil do usuário */
-export const post_user_profile_edit = async (
-  req: Request<{ username: string }>,
-  res: Response,
-  next: NextFunction
-) => {
-  return res.send(`USER "${req.params.username}" PROFILE UPDATED`);
-};
+export const post_user_profile_edit = [
+  body('firstName')
+    .trim()
+    .escape()
+    .isLength({ min: 2, max: 30 })
+    .withMessage('O nome deve ter de 2 a 30 carcteres'),
+
+  body('lastName')
+    .trim()
+    .escape()
+    .isLength({ min: 2, max: 30 })
+    .withMessage('O sobrenome deve ter de 2 a 30 carcteres'),
+
+  async (
+    req: Request<
+      { username: string },
+      {},
+      { firstName: string; lastName: string }
+    >,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (req.params.username !== req.user!.username) {
+      return res.render('errorPage', {
+        error: {
+          status: 403,
+          message: 'Você não tem permissões para editar esse usuário',
+        },
+      });
+    }
+
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.render('users/editProfile', {
+          formData: req.body,
+          errors: errors.array(),
+        });
+      }
+
+      const newInfo = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      };
+      await User.updateOne({ username: req.params.username }, newInfo, {});
+
+      return res.redirect('/users/' + req.user?.username);
+    } catch (error) {
+      return next(error);
+    }
+  },
+];
